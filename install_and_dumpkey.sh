05a52c57385c31bdfae621d2b54e655f24388922
shc0228 <45022967+shc0228@users.noreply.github.com>
2018-11-14 10:36:43 +0800

    Update install_and_dumpkey.sh

--- install_and_dumpkey.sh
+++ install_and_dumpkey.sh
@@ -1,3 +1,3 @@
 dpkg --configure -a
-curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/dump_key.sh | sh
+curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/dump_key.sh | sh
