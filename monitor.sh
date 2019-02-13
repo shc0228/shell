7d37f3bb12140df69855ab8b149195532e0b2df0
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-13 22:15:02 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,11 +5,11 @@ do
         process=`ps -ef| grep chain33 | grep -v grep`; #查询mysqld进程，grep -v grep去掉grep进程
  
         if [ "$process" == "" ]; then
-                sleep 1;
+                sleep 10;
                 echo "process 不存在,开始执行";
-         
+                
         else
                 echo "process exsits";
-                break;
+                
         fi
 done
