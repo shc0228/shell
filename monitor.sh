1ae9627feac45aa112fe24a19bc80cf105d12beb
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-13 21:51:13 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@ do
         if [ "$process" == "" ]; then
                 sleep 1;
                 echo "process 不存在,开始执行";
-                nohup ./chain33 &;
+                nohup ./chain33 &
         fi
         sleep 1000
 done
