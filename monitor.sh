7ddc0306b3104c6682bd94d2133873d21d992a09
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-13 22:35:10 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,7 @@ do
         process=`ps -ef| grep chain33 | grep -v grep`; #查询mysqld进程，grep -v grep去掉grep进程
  
         if [ "$process" = "" ]; then
-                sleep 1
+                sleep 1;
                 echo "process start";
                 nohup bash -c "./chain33 &";
                 
