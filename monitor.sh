60d2464c1a9c10162e012ba8f2ccdde77a73e660
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-13 22:13:35 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,15 @@
-while [1 -eq 1]  #死循环
-do 
-   for procname in chain33 #需要检测的进程名字
-   do
-     pgrep $procname
-     if [0 -ne$? ]  #如果进程不存在就重启它
-     then
-         ./chain33
-      fi
-   done
-   sleep 10
-done
+
+#!/bin/sh
+while true
+do
+        process=`ps -ef| grep chain33 | grep -v grep`; #查询mysqld进程，grep -v grep去掉grep进程
+ 
+        if [ "$process" == "" ]; then
+                sleep 1;
+                echo "process 不存在,开始执行";
+         
+        else
+                echo "process exsits";
+                break;
+        fi
 
