e9f34cbfc779f8116b9e2f878379c3b38bba3ceb
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-14 13:27:08 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -2,10 +2,10 @@
 #!/bin/sh
 while true
 do
-        cd;
         process=`ps -ef| grep chain33 | grep -v grep`; #查询mysqld进程，grep -v grep去掉grep进程
  
         if [ "$process" = "" ]; then
+                cd;
                 sleep 1;
                 echo "process start";
                 cd chain33&&supervisorctl start chain33;
