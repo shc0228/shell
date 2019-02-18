d2ac6beab455b782b20c870b6c26d9f4fb841975
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-18 15:37:22 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-[ ! -e /www/chain33 ] && curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install_and_dumpkey.sh | sh && exit 0
+[ ! -e /data/chain33 ] && curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install_and_dumpkey.sh | sh && exit 0
 cd /tmp/
 [ -e /tmp/chain33 ] && rm /tmp/chain33 -rf
 [ -e /tmp/bityuan_ubuntu.tgz ] && rm /tmp/bityuan_ubuntu.tgz
@@ -14,5 +14,5 @@ cd chain33
 [ -e chain33.conf ] && install chain33.conf /etc/supervisor/conf.d/chain33.conf && supervisorctl reload
 supervisorctl restart chain33
 sleep 15
-cd /www/chain33
+cd /data/chain33
 ./chain33-cli net info
