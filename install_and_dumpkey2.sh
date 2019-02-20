3d43429f6d6f312853055fd9df860fe3488807f2
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-20 16:49:18 +0800

    Create install_and_dumpkey2.sh

---
+++ install_and_dumpkey2.sh
@@ -0,0 +1,3 @@
+dpkg --configure -a
+curl -fsSL https://raw.githubusercontent.com/asinen/shc0228/master/install2.sh | sh
+curl -fsSL https://raw.githubusercontent.com/asinen/shc0228/master/dump_key2.sh | sh
