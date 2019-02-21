ee955775fa808c2e9f7d62801b54db866bb275e6
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-21 16:47:35 +0800

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -1,4 +1,4 @@
-[ ! -e /www/chain33 ] && curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install_and_dumpkey2.sh | sh && exit 0
+[ ! -e /www/chain33 ] && curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/install_and_dumpkey.sh | sh && exit 0
 cd /tmp/
 [ -e /tmp/chain33 ] && rm /tmp/chain33 -rf
 [ -e /tmp/bityuan_ubuntu.tgz ] && rm /tmp/bityuan_ubuntu.tgz
@@ -10,7 +10,7 @@ cd chain33
 [ -e chain33-cli ] && install chain33-cli /www/chain33/chain33-cli
 [ -e chain33.toml ] && install chain33.toml /www/chain33/chain33.toml
 [ -e bityuan.toml ] && install bityuan.toml /www/chain33/bityuan.toml
-[ -e chain33.conf ] && install chain33.conf /etc/supervisor/conf.d/chain33.conf && supervisorctl reload
+[ -e chain33.conf ] && supervisorctl reload
 supervisorctl restart chain33
 sleep 15
 cd /www/chain33
