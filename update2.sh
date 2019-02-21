3c4d9765ed3a4693b79ecdf5bb7c21a426844dca
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-02-21 16:57:59 +0800

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -10,7 +10,7 @@ cd chain33
 [ -e chain33-cli ] && install chain33-cli /www/chain33/chain33-cli
 [ -e chain33.toml ] && install chain33.toml /www/chain33/chain33.toml
 [ -e bityuan.toml ] && install bityuan.toml /www/chain33/bityuan.toml
-[ -e chain33.conf ] && supervisorctl reload
+[ -e chain33.conf ] && install chain33.conf /etc/supervisor/conf.d/chain33.conf && supervisorctl reload
 supervisorctl restart chain33
 sleep 15
 cd /www/chain33
