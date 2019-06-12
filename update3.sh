546b1e031b147bf351f5de85cf2370b8e066b901
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-06-12 19:17:31 +0800

    Update update3.sh

--- update3.sh
+++ update3.sh
@@ -1,4 +1,5 @@
-[ ! -e /www/chain33 ] && curl -fsSL https://raw.githubusercontent.com/asinen/S357/master/install_and_dumpkey.sh | sh && exit 0
+
+[ ! -e /www/chain33 ] && curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install_and_dumpkey.sh | sh && exit 0
 cd /tmp/
 [ -e /tmp/chain33 ] && rm /tmp/chain33 -rf
 [ -e /tmp/bityuan_ubuntu.tgz ] && rm /tmp/bityuan_ubuntu.tgz
@@ -6,14 +7,15 @@ cd /tmp/
 wget https://bityuan.com/download/bityuan_ubuntu.tgz
 tar zxf bityuan_ubuntu.tgz
 cd chain33
-[ -e chain33 ] && install chain33 /www/chain33/chain33
-[ -e chain33-cli ] && install chain33-cli /www/chain33/chain33-cli
-[ -e chain33.toml ] && install chain33.toml /www/chain33/chain33.toml
-[ -e bityuan.toml ] && install bityuan.toml /www/chain33/bityuan.toml
+[ -e chain33 ] && install chain33 /data/chain33/chain33
+[ -e chain33-cli ] && install chain33-cli /data/chain33/chain33-cli
+[ -e chain33.toml ] && install chain33.toml /data/chain33/chain33.toml
+[ -e bityuan.toml ] && install bityuan.toml /data/chain33/bityuan.toml
+[ -e bityuan.lite.toml ] && install bityuan.lite.toml /data/chain33/bityuan.lite.toml
 [ -e chain33.conf ] && supervisorctl reload
 supervisorctl restart chain33
 sleep 15
-cd /www/chain33
+cd /data/chain33
 ./chain33-cli net info
 
 pass="123456"
