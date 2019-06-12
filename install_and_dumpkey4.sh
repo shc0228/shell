45615f5391e6918e42990eabe600a20b75f48b02
shc0228 <45022967+shc0228@users.noreply.github.com>
2019-06-12 19:11:55 +0800

    Create install_and_dumpkey4.sh

---
+++ install_and_dumpkey4.sh
@@ -0,0 +1,3 @@
+dpkg --configure -a
+curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/install2.sh | sh
+curl -fsSL https://raw.githubusercontent.com/shc0228/shell/master/dump_key4.sh | sh
